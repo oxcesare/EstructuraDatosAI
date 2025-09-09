30b25a3619501f3178b5f11b8109f4718c1b59a8
César <cesar180687@hotmail.com>
2025-09-09 10:16:28 -0600

    Ejercicio recomendacion

---
+++ src/com.mx.curso/unidad1/matriz/recomendacion_peliculas.py
@@ -0,0 +1,38 @@
+# Matriz de recomendacion de peliculas 
+
+matriz = [
+
+    [5,	5,	4,	5,	5,	1,	5,	5,	3,	5,	3,	4,	4],
+    [2,	5,	4,	5,	5,	3,	5,	4,	5,	5,	3,	5,	3],
+    [2,	5,	5,	5,	4,	3,	5,	4,	4,	5,	2,	4,	3],
+    [4,	5,	4,	5,	5,	4,	4,	5,	5,	5,	3,	4,	2],
+    [5,	4,	4,	5,	5,	4,	5,	5,	3,	5,	1,	5,	1],
+    [3,	5,	3,	5,	3,	5,	2,	1,	3,	5,	3,	1,	1],
+    [2,	4,	5,	3,	5,	2,	5,	3,	3,	4,	1,	3,	2],
+    [4,	5,	5,	2,	5,	1,	3,	5,	5,	4,	3,	1,	3],
+    [3,	2,	5,	2,	5,	2,	3,	2,	5,	4,	5,	4,	2],
+    [3,	5,	2,	5,	3,	1,	1,	5,	1,	5,	3,	1,	5],
+    [5,	1,	5,	5,	5,	4,	4,	1,	4,	5,	4,	1,	5],
+    [5,	5,	5,	5,	5,	5,	5,	5,	5,	5,	5,	5,	5],
+    [5,	4,	3,	5,	5,	1,	4,	2,	3,	5,	2,	1,	5]
+]
+
+#imprimir la matriz de calificaciones
+for i in range(len(matriz)):
+    for j in range(len(matriz[i])):
+        print(matriz[i][j], end="")
+    print()    
+
+#Promedio de una pelicula 
+suma=0
+pelicula=9
+
+for i in range(len(matriz)):
+    suma +=  int(matriz[i][pelicula])
+
+print("la suma de calificaciones pelicula 1 ", suma) 
+promedio = suma / len(matriz)
+print("El promedio  de calificaciones pelicula 1 ", promedio) 
+
+calificacionesUsuario= matriz[6]
+print("Calificaciones usuario 0 ", calificacionesUsuario) 
