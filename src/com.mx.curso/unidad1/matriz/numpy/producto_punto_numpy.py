ae9fa1c7f32f252da21b6000d609822302eac41b
César <cesar180687@hotmail.com>
2025-09-12 12:41:06 -0600

    Ejercicios Redes Neuronales

--- src/com.mx.curso/unidad1/matriz/numpy/producto_punto_numpy.py
+++ src/com.mx.curso/unidad1/matriz/numpy/producto_punto_numpy.py
@@ -1,6 +1,7 @@
 #Operación de producto punto (base de las redes neuronales)
 
 import numpy as np
+import time 
 
 # Vector de entrada (una sola característica de 3 neuronas)
 entrada = np.array([1,4])
@@ -11,8 +12,13 @@ pesos = np.array([
     [0.1, 0.9,0.10,1.1]
 ])
 
+start_time = time.time()
 # Realizar el producto punto
 salida = np.dot(entrada, pesos)
+end_time = time.time()
+
+print("Incio :", start_time)
+print("Tiempos :", end_time-start_time)
 print("Vector de entrada:", entrada)
 print("Matriz de pesos:\n", pesos)
 print("\nSalida de la capa de neuronas (producto punto):", salida)
